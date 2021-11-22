84c6a9b72779bd8f6067a148e14c1f4fec3838c7
DavidEsdrs <carpina1983@gmail.com>
2021-11-22 12:00:38 -0300

    Minify

--- src/routes.ts
+++ src/routes.ts
@@ -19,117 +19,77 @@ import {
 
 const routes = Router();
 
-class Routes {
-    /**
-     * Route to create a new user
-     */
-    postNewUser() {
-        const newAccountController = new NewAccountController();
+/**
+ * Route to create a new user
+ */
+const newAccountController = new NewAccountController();
 
-        routes.post('/user/new', newAccountController.handle);
-    }
+routes.post('/user/new', newAccountController.handle);
 
-    /**
-     * Route to client login
-     */
-    postLogin() {
-        const loginController = new LoginController();
-        
-        routes.post('/login', loginController.handle);
-    }
+/**
+ * Route to client login
+ */
+const loginController = new LoginController();
 
-    /**
-     * Route to post a new appointment
-     */
-    postNewSchedule() {
-        const scheduleController = new ScheduleController();
+routes.post('/login', loginController.handle);
 
-        routes.post('/user/schedules/new', ensureAuthenticatedUser, scheduleController.handle);
-    }
+/**
+ * Route to post a new appointment
+ */
+const scheduleController = new ScheduleController();
 
-    /**
-     * Route available only for admins to delete all the appointments
-     */
-    deleteAllSchedulesAdmin() {
-        const deleteAllController = new DeleteAllController();
+routes.post('/user/schedules/new', ensureAuthenticatedUser, scheduleController.handle);
 
-        routes.delete('/user/schedules', ensureAuthenticatedUser, ensureAdmin, deleteAllController.handle);
-    }
+/**
+ * Route available only for admins to delete all the appointments
+ */
+const deleteAllController = new DeleteAllController();
 
-    /**
-     * Route available only for admins for get the schedule
-     */
-    getAllSchedules() {
-        const getSchedulesController = new GetSchedulesController();
+routes.delete('/user/schedules', ensureAuthenticatedUser, ensureAdmin, deleteAllController.handle);
 
-        routes.get('/admin/schedules', ensureAuthenticatedUser, ensureAdmin, getSchedulesController.handler);
-    }
+/**
+ * Route available only for admins for get the schedule
+ */
+const getSchedulesController = new GetSchedulesController();
 
-    /**
-     * Route available only for admins for change any appointment situation
-     */
-    putChangeScheduleAdmin() {
-        const adminChangeScheduleController = new AdminChangeScheduleController();
+routes.get('/admin/schedules', ensureAuthenticatedUser, ensureAdmin, getSchedulesController.handler);
 
-        routes.put('/admin/schedules', ensureAuthenticatedUser, ensureAdmin, adminChangeScheduleController.handle);
-    }
+/**
+ * Route available only for admins for change any appointment situation
+ */
+const adminChangeScheduleController = new AdminChangeScheduleController();
 
-    /**
-     * Route to cancel an appointment
-     */
-    putChangeSchedule() {
-        const changeScheduleController = new ChangeScheduleController();
+routes.put('/admin/schedules', ensureAuthenticatedUser, ensureAdmin, adminChangeScheduleController.handle);
 
-        routes.put('/user/schedules/:id', ensureAuthenticatedUser, changeScheduleController.handle);
-    }
+/**
+ * Route to cancel an appointment
+ */
+const changeScheduleController = new ChangeScheduleController();
 
-    /**
-     * Route to get all appointments of the user
-     */
-    getMySchedules() {
-        const getMySchedulesController = new GetMySchedulesController();
+routes.put('/user/schedules/:id', ensureAuthenticatedUser, changeScheduleController.handle);
 
-        routes.get('/user/schedules', ensureAuthenticatedUser, getMySchedulesController.handle);
-    }
+/**
+ * Route to get all appointments of the user
+ */
+const getMySchedulesController = new GetMySchedulesController();
 
-    /**
-     * Route to get a specific appointment
-     */
-    getScheduleInfo() { 
-        const getScheduleInfoController = new GetScheduleInfoController();
+routes.get('/user/schedules', ensureAuthenticatedUser, getMySchedulesController.handle);
 
-        routes.get('/user/schedules/:id', ensureAuthenticatedUser, getScheduleInfoController.handle);
-    }
+/**
+ * Route to get a specific appointment
+ */
+const getScheduleInfoController = new GetScheduleInfoController();
 
-    getSearchSchedule() {
-        const searchScheduleController = new SearchScheduleController();
+routes.get('/user/schedules/:id', ensureAuthenticatedUser, getScheduleInfoController.handle);
 
-        routes.get('/admin/schedules/search', ensureAuthenticatedUser, searchScheduleController.handle);
-    }
+const searchScheduleController = new SearchScheduleController();
 
-    setObservers() {
-        const oneDay = 86400000;
+routes.get('/admin/schedules/search', ensureAuthenticatedUser, searchScheduleController.handle);
 
-        setInterval(deleteCancelledAppointments, oneDay);
-        setInterval(updateSchedulesSituation, oneDay);
-        setInterval(updateScheduleToConcluded, oneDay);
-    }
+const oneDay = 86400000;
 
-    constructor() {
-        this.postNewUser();
-        this.postLogin();
-        this.postNewSchedule();
-        this.deleteAllSchedulesAdmin();
-        this.getAllSchedules();
-        this.putChangeSchedule();
-        this.putChangeScheduleAdmin();
-        this.getMySchedules();
-        this.getScheduleInfo();
-        this.setObservers();
-        this.getSearchSchedule();
-    }
-}
-
-new Routes();
+setInterval(deleteCancelledAppointments, oneDay);
+setInterval(updateSchedulesSituation, oneDay);
+setInterval(updateScheduleToConcluded, oneDay);
 
 export { routes }
